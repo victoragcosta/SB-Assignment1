fd32bc8d485c9c69c160ad7b58f5c39e52758679
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2018-09-24 20:48:20 -0300

    Added initial source files, makefiles and folders.

---
+++ Ligador/Ligador.cpp
@@ -0,0 +1,15 @@
+// Software básico - Trabalho 02 - Ligador
+
+// Includes:
+#include <iostream>
+#include <string>
+
+// Namespace:
+using namespace std;
+
+// Main function:
+int main(int argc, char const *argv[]) {
+
+  return 0;
+  
+}
---
+++ Montador/Montador.cpp
@@ -0,0 +1,23 @@
+// Software básico - Trabalho 02 - Montador
+
+// Includes:
+#include <iostream>
+#include <string>
+
+// Namespace:
+using namespace std;
+
+// Main function:
+int main(int argc, char const *argv[]) {
+
+  map <string, int> symbolTable;
+
+  if(argc != 2) {
+      cerr << "Incorrect number of files given to function." << endl;
+      cerr << "Exiting!" << endl;
+      exit(1);
+  }
+
+  return 0;
+
+}
